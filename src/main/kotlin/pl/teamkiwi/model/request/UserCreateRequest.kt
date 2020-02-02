f180f711541a90d0f9e47b005bac4ec3ac56f77d
dekad300 <bakowskiradek1@gmail.com>
2020-02-02 02:02:15 +0100

    added valiktor library and created basic user validation

--- src/main/kotlin/pl/teamkiwi/model/request/UserCreateRequest.kt
+++ src/main/kotlin/pl/teamkiwi/model/request/UserCreateRequest.kt
@@ -1,8 +1,22 @@
 package pl.teamkiwi.model.request
 
+import org.valiktor.ConstraintViolationException
+import org.valiktor.functions.isEmail
+import org.valiktor.functions.isNotBlank
+import org.valiktor.validate
+
 class UserCreateRequest(
     val email: String,
     val username: String,
     val description: String?,
     val password: String
-)
+) {
+    init {
+        //todo catch exception and throw custom message
+        validate(this) {
+            validate(UserCreateRequest::email).isNotBlank().isEmail()
+            validate(UserCreateRequest::username).isNotBlank()
+            validate(UserCreateRequest::password).isNotBlank()
+        }
+    }
+}
